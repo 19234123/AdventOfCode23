86d6cfcf1c87d1532f493cd923fc18af09867434
Steven <19234123@brookes.ac.uk>
2023-12-02 17:25:23 +0000

    Day 2

--- main.cpp
+++ main.cpp
@@ -3,146 +3,158 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <sstream>
 
 using std::vector;
 using std::string;
 using std::cout;
-
-vector<string> readFile();
-int lineValue(const string& line);
-string convertStringToDigit(const string& line);
-string convertStringToDigitOrg(const string& line);
+using std::endl;
 
 string filePath = R"(C:\Dev\Text_files\input.txt)";
+vector<string> readFile();
+vector<string> splitLine(const string& line, const char& delimiter);
+string trim(const string& str);
 
-
+// part 2
 
 int main() {
     vector<string> rawInput = readFile();
 
-    int sum = 0;
-    for (auto const &line: rawInput){
-        // Part 1
-        //sum += lineValue(line);
+    string roundDataRaw;
+    int minimumCubes;
+    int sumCubes = 0;
 
-        // Part 2
-        string lineAsDigits = convertStringToDigit(line);
-        sum += lineValue(lineAsDigits);
+    for (auto const &line: rawInput){
+        string delimiter = ":";
+        int delimiterIndex = line.find(delimiter);
+
+        // round data
+        roundDataRaw = line.substr(delimiterIndex+2);
+        vector<string> gameSplit = splitLine(roundDataRaw, ';');
+
+        int maxRed = 1;
+        int maxGreen = 1;
+        int maxBlue = 1;
+
+        vector<string> currentRoundSplit;
+        for (const auto &game: gameSplit) {
+            currentRoundSplit = splitLine(game, ',');
+
+            // current round
+            for (const auto &round: currentRoundSplit) {
+
+                vector<string> currentRoundData = splitLine(round, ' ');
+                int cubeCount = std::stoi(currentRoundData.at(0));
+                string cubeColour = currentRoundData.at(1);
+
+                if (cubeColour == "red") {
+                    if (cubeCount > maxRed){
+                        maxRed = cubeCount;
+                    }
+                } else if (cubeColour == "green") {
+                    if (cubeCount > maxGreen){
+                        maxGreen = cubeCount;
+                    }
+                } else if (cubeColour == "blue") {
+                    if (cubeCount > maxBlue){
+                        maxBlue = cubeCount;
+                    }
+                }
+            }
+        }
+        minimumCubes = maxRed * maxBlue * maxGreen;
+        sumCubes += minimumCubes;
     }
 
-    std::cout << sum;
+    cout << sumCubes;
 
     return 0;
 }
 
+// Part 1
+//int main() {
+//    vector<string> rawInput = readFile();
+//
+//    string gameNumberRaw;
+//    int gameNumber;
+//    string roundDataRaw;
+//    int gameIDSum = 0;
+//
+//    for (auto const &line: rawInput){
+//        bool validGame = true;
+//        string delimiter = ":";
+//        int delimiterIndex = line.find(delimiter);
+//
+//        // game number
+//        gameNumberRaw = line.substr(0, delimiterIndex);
+//        gameNumber = std::stoi(gameNumberRaw.substr(5));
+//
+//        // round data
+//        roundDataRaw = line.substr(delimiterIndex+2);
+//        vector<string> gameSplit = splitLine(roundDataRaw, ';');
+//
+//        vector<string> currentRoundSplit;
+//        for (const auto &game: gameSplit) {
+//            currentRoundSplit = splitLine(game, ',');
+//
+//            int redCubes = 0;
+//            int greenCubes = 0;
+//            int blueCubes = 0;
+//
+//            // current round
+//            for (const auto &round: currentRoundSplit) {
+//
+//                vector<string> currentRoundData = splitLine(round, ' ');
+//                int cubeCount = std::stoi(currentRoundData.at(0));
+//                string cubeColour = currentRoundData.at(1);
+//
+//                if (cubeColour == "red") {
+//                    redCubes = cubeCount;
+//                } else if (cubeColour == "green") {
+//                    greenCubes = cubeCount;
+//                } else if (cubeColour == "blue") {
+//                    blueCubes = cubeCount;
+//                }
+//            }
+//
+//            if (redCubes > 12 || greenCubes > 13 || blueCubes > 14) {
+//                validGame = false;
+//                break;
+//            }
+//        }
+//
+//        if (validGame){
+//            gameIDSum += gameNumber;
+//        }
+//
+//    }
+//
+//    cout << gameIDSum;
+//
+//    return 0;
+//}
 
-string convertStringToDigit(const string& line){
-    std::map<string, char> digitMap;
-    digitMap["one"] = '1';
-    digitMap["two"] = '2';
-    digitMap["thr"] = '3';
-    digitMap["fou"] = '4';
-    digitMap["fiv"] = '5';
-    digitMap["six"] = '6';
-    digitMap["sev"] = '7';
-    digitMap["eig"] = '8';
-    digitMap["nin"] = '9';
+vector<string> splitLine(const string& line, const char& delimiter){
+    vector<string> splitString;
 
-    string digits;
-    for (int i=0; i<line.size(); i++){
-        char currentChar = line[i];
+    std::stringstream stream(line);
 
-        if (isdigit(currentChar)){
-            digits += currentChar;
-        } else {
-            string testString = line.substr(i, 3);
-            auto iterator = digitMap.find(testString);
-            if (iterator != digitMap.end()) {
-                digits += iterator->second;
-            }
+    string currentLine;
+    while (stream.good()) {
+        std::getline(stream, currentLine, delimiter);
+        if (!currentLine.empty()) {
+            splitString.push_back(trim(currentLine));
         }
     }
-    return digits;
+
+    return splitString;
 }
 
-
-string convertStringToDigitOrg(const string& line){
-    string digits;
-    for (int i=0; i<line.size(); i++){
-        char currentChar = line[i];
-
-        if (isdigit(currentChar)){
-            digits += currentChar;
-        } else {
-            char nextChar = line[i+1];
-            char nextNextChar = line[i+2];
-            switch (currentChar){
-                case 'o':
-                    if (nextChar == 'n' && nextNextChar == 'e'){
-                        digits += '1';
-                    }
-                    break;
-                case 't':
-                    if (nextChar == 'w' && nextNextChar == 'o'){
-                        digits += '2';
-                    } else if (nextChar == 'h' && nextNextChar == 'r'){
-                        digits += '3';
-                    }
-                    break;
-                case 'f':
-                    if (nextChar == 'o' && nextNextChar == 'u'){
-                        digits += '4';
-                    } else if (nextChar == 'i' && nextNextChar == 'v'){
-                        digits += '5';
-                    }
-                    break;
-                case 's':
-                    if (nextChar == 'i' && nextNextChar == 'x'){
-                        digits += '6';
-                    } else if (nextChar == 'e' && nextNextChar == 'v'){
-                        digits += '7';
-                    }
-                    break;
-                case 'e':
-                    if (nextChar == 'i' && nextNextChar == 'g'){
-                        digits += '8';
-                    }
-                    break;
-                case 'n':
-                    if (nextChar == 'i' && nextNextChar == 'n'){
-                        digits += '9';
-                    }
-                    break;
-            }
-        }
-    }
-
-    return digits;
-}
-
-
-int lineValue(const string& line){
-    char firstDigit = '\0';
-    char lastDigit = '\0';
-    for (char currentChar : line){
-        if (isdigit(currentChar)){
-            if (firstDigit){
-                lastDigit = currentChar;
-            } else {
-                firstDigit = currentChar;
-            }
-        }
-    }
-
-    if (!lastDigit){
-        lastDigit = firstDigit;
-    }
-
-    string lineNumber;
-    lineNumber += firstDigit;
-    lineNumber += lastDigit;
-
-    return std::stoi(lineNumber);
+string trim(const string& str)
+{
+    size_t first = str.find_first_not_of(' ');
+    size_t last = str.find_last_not_of(' ');
+    return str.substr(first, (last-first+1));
 }
 
 

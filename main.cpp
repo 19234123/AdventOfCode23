5a8e77806d2fe4e31c28ec98a0ca07a5ec41b6ae
19234123 <115121683+19234123@users.noreply.github.com>
2023-12-06 17:40:49 +0000

    Day 6 - Part 2

--- main.cpp
+++ main.cpp
@@ -14,39 +14,34 @@ string filePath = R"(C:\Dev\Text_files\input.txt)";
 int main() {
     vector<string> rawInput = readFile(filePath);
 
-    // Setup round data list
-    vector<vector<int>> roundDataList;
-    auto dataSize = splitLineToString(rawInput[0], ' ').size();
-    for (int i=0; i<dataSize-1; i++) {
-        roundDataList.emplace_back();
-    }
+    vector<string> roundData;
 
     for (auto& line: rawInput) {
         auto lineData = splitLineToString(line, ' ');
+        string roundDataString;
         for (int i=1; i<lineData.size(); i++){
-            roundDataList[i-1].push_back(std::stoi(lineData[i]));
+            roundDataString += lineData[i];
+        }
+        roundData.push_back(roundDataString);
+    }
+
+    long long totalCount = 1;
+    long long maxTime = std::stoi(roundData[0]);
+    long long maxDistance = std::stoll(roundData[1]);
+    long long distanceTravelled;
+    long long roundCount = 0;
+
+    for (int secondsHeld=1; secondsHeld<=maxTime/2; secondsHeld++) {
+        distanceTravelled = secondsHeld * (maxTime-secondsHeld);
+        if (distanceTravelled > maxDistance) {
+            roundCount++;
         }
     }
 
-    int totalCount = 1;
-    for (const auto& round: roundDataList) {
-        int maxTime = round[0];
-        int maxDistance = round[1];
-        int distanceTravelled;
-        int roundCount = 0;
-
-        for (int secondsHeld=1; secondsHeld<=maxTime/2; secondsHeld++) {
-            distanceTravelled = secondsHeld * (maxTime-secondsHeld);
-            if (distanceTravelled > maxDistance) {
-                roundCount++;
-            }
-        }
-
-        if (maxTime % 2 != 0){
-            totalCount *= 2 * roundCount;
-        } else {
-            totalCount *= (2 * roundCount) - 1;
-        }
+    if (maxTime % 2 != 0){
+        totalCount *= 2 * roundCount;
+    } else {
+        totalCount *= (2 * roundCount) - 1;
     }
 
     cout << totalCount;
